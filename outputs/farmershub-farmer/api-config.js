995c74daf39b4a729eae169810d23f4768468de1
kothapallikarunya2007-cmd <kothapallikarunya2007@gmail.com>
2026-09-17 15:26:52 +0530

    Configure deployed Render API URL

--- outputs/farmershub-farmer/api-config.js
+++ outputs/farmershub-farmer/api-config.js
@@ -1,2 +1,1 @@
-// Set this to the public URL of the deployed Flask API before deploying.
-window.FARMERSHUB_API_URL = '';
+window.FARMERSHUB_API_URL = 'https://farmer-marketplace-1mcz.onrender.com';
